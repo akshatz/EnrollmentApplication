d36e8ee1df1236d60829e5d486641d7d1f6791ae
Akshat Zala <akshatzala@gmail.com>
2020-02-16 16:21:49 +0530

    Enrollment Template

--- application/routes.py
+++ application/routes.py
@@ -1,5 +1,6 @@
 from application import app
 from flask import render_template
+from flask import request
 
 @app.route("/")
 @app.route("/home")
@@ -11,22 +12,22 @@ def home():
 def login():
     return render_template('login.html',login=True)
 
-@app.route("/courses")
-def courses():
+@app.route("/courses/")
+@app.route("/courses/<term>/")
+def courses(term=2020):
     courseData= [{
-        "courseId":"1111","title":"Django",
-        "desc": "Intro to Django","credits":"5", "term":"Fall, Spring"}, 
-        {"courseId":"2222", "title":"Flask",
-        "desc":"Intro to Python-Flask", "credits":"4","term":"Spring",
-        },{"courseId":"3333","title":".Net",
-        "desc": "Intro to Django","credits":"5","term":"Fall, Spring"},
-        {"courseId":"2222", "title":"C#",
-        "desc":"Intro to Python-Flask", "credits":"4","term":"Spring",
-        },{"courseId":"5555","title":"Java",
-        "desc": "Intro to Django","credits":"5","term":"Fall, Spring"},
-        {"courseId":"6666", "title":"C","desc":"Intro to Python-Flask", "credits":"4","term":"Spring"}]
-    print(courseData[0]["title"])
-    return render_template('courses.html', courseData=courseData, courses=True)
+        "courseID":"1111","title":"Django",
+        "description": "Intro to Django","credits":"5", "term":"Fall, Spring"}, 
+        {"courseID":"2222", "title":"Python-Flask",
+        "description":"Intro to Python-Flask", "credits":"4","term":"Spring",
+        },{"courseID":"3333","title":".Net",
+        "description": "Intro to .Net","credits":"5","term":"Fall, Spring"},
+        {"courseID":"4444", "title":"C#",
+        "description":"Intro to C#", "credits":"4","term":"Spring",
+        },{"courseID":"5555","title":"Java",
+        "description": "Intro to Java","credits":"5","term":"Fall, Spring"},
+        {"courseID":"6666", "title":"C","description":"Intro to C", "credits":"4","term":"Spring"}]
+    return render_template('courses.html', courseData=courseData, courses=True, term=term)
 
 @app.route("/register")
 def register():
@@ -34,4 +35,7 @@ def register():
 
 @app.route('/enrollment')
 def enrollment():
-    return render_template('enrollment.html',enrollment=True)
+    id = request.args.get("courseID")
+    title = request.args.get("title")
+    term = request.args.get("term")
+    return render_template('enrollment.html',enrollment=True, data={"id":id,"title": title,"term": term})
