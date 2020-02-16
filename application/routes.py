88bbda17990d895c2a3dd6cff7926a10a937dce6
Akshat Zala <akshatzala@gmail.com>
2020-02-16 15:33:33 +0530

    Added active feature in nav bar

--- application/routes.py
+++ application/routes.py
@@ -2,20 +2,36 @@ from application import app
 from flask import render_template
 
 @app.route("/")
-@app.route("/index")
 @app.route("/home")
-def index():
-    return render_template('index.html',login=False)
+def home():
+    return render_template('index.html',home=True)
 
-@app.route("/courses")
-def courses():
-    return render_template('courses.html',login=False)
-
-@app.route("/register")
-def register():
-    return render_template('register.html',login=False)
 
 @app.route("/login")
 def login():
-    return render_template('login.html')
+    return render_template('login.html',login=True)
 
+@app.route("/courses")
+def courses():
+    courseData= [{
+        "courseId":"1111","title":"Django",
+        "desc": "Intro to Django","credits":"5", "term":"Fall, Spring"}, 
+        {"courseId":"2222", "title":"Flask",
+        "desc":"Intro to Python-Flask", "credits":"4","term":"Spring",
+        },{"courseId":"3333","title":".Net",
+        "desc": "Intro to Django","credits":"5","term":"Fall, Spring"},
+        {"courseId":"2222", "title":"C#",
+        "desc":"Intro to Python-Flask", "credits":"4","term":"Spring",
+        },{"courseId":"5555","title":"Java",
+        "desc": "Intro to Django","credits":"5","term":"Fall, Spring"},
+        {"courseId":"6666", "title":"C","desc":"Intro to Python-Flask", "credits":"4","term":"Spring"}]
+    print(courseData[0]["title"])
+    return render_template('courses.html', courseData=courseData, courses=True)
+
+@app.route("/register")
+def register():
+    return render_template('register.html', register=True)
+
+@app.route('/enrollment')
+def enrollment():
+    return render_template('enrollment.html',enrollment=True)
