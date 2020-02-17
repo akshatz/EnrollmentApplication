19ccf2dc288f1a4ce08f9d5944bf4113a5977950
Akshat Zala <akshatzala@gmail.com>
2020-02-17 12:57:35 +0530

    Set up database

--- config.py
+++ config.py
@@ -2,3 +2,7 @@ import os
 
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET KEY') or "secret_string"
+
+    MONGODB_SETTINGS ={'db':'Enrollment',
+        'host':'mongodb://127.0.0.1:27001/Enrollment'
+    }
